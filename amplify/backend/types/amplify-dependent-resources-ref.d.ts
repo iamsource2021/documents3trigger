c8e9925c40284f9b0e5f73a0e4a727c8d40bf176
Eduardo Ortiz <eduardooa1980@gmail.com>
2022-10-19 17:57:10 -0500

    repo init

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,10 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "documents3trigger22cecc07": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
